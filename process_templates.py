3fadebc4bcf621b59680302d6d3d926824b739a8
welvico <veryok-abc@yahoo.com>
2024-06-26 21:56:04 +0800

    Update process_templates.py

--- process_templates.py
+++ process_templates.py
@@ -1,57 +1,33 @@
 import os
-import re
 
-template_dir = 'template'
-result_dir = 'result'
+def replace_ip_in_template(ip_file, template_file, result_file):
+    with open(ip_file, 'r') as ip_f, open(template_file, 'r') as template_f:
+        ip_lines = ip_f.readlines()
+        template_content = template_f.read()
 
-# 1. 替换模板中的占位符
-def replace_template(filename):
+    replaced_content = template_content
+    for line in ip_lines:
+        ip_port = line.strip()
+        replaced_content = replaced_content.replace('ipipip', ip_port, 1)
 
-    template_file = os.path.join(template_dir, filename) 
-    content_file = filename  
+    with open(result_file, 'w') as result_f:
+        result_f.write(replaced_content)
 
-    with open(template_file, 'r') as f:
-        template = f.read()
-        
-    with open(os.path.join('ip', content_file), 'r') as f:
-        content = f.read()
-
-    domain_port = re.findall(r'http://\S+', content)  
-    for dp in domain_port:
-        template = template.replace('ipipip', dp, 1)
-
-    result_file = os.path.join(result_dir, content_file)
-    with open(result_file, 'w') as f:
-        f.write(template)
-        
-# 2. 合并相同分组        
-def merge_by_group(genre):
-
-    result = ''
-    for root, dirs, files in os.walk(result_dir):
-        for file in files:
-            if file.startswith(genre):
-                with open(os.path.join(root, file), 'r') as f:  
-                    result += f.read() + '\n'
-                    
-    result_file = os.path.join(result_dir, 'all.txt')            
-    with open(result_file, 'a') as f:
-        f.write(genre + '#genre#\n') 
-        f.write(result)
-        
-# 3. 主函数
 def main():
+    # Paths to your files
+    ip_fujian = 'ip/Fujian_114.txt'
+    template_fujian = 'template/Fujian_114.txt'
+    result_fujian = 'result/Fujian_114.txt'
 
-  for filename in os.listdir(template_dir):
-    
-    if filename.endswith(".txt"):  
+    ip_henan = 'ip/Henan_327.txt'
+    template_henan = 'template/Henan_327.txt'
+    result_henan = 'result/Henan_327.txt'
 
-      replace_template(filename)
+    # Process Fujian files
+    replace_ip_in_template(ip_fujian, template_fujian, result_fujian)
 
-  genres = ['CCTV', '山东卫视', '快乐垂钓']
+    # Process Henan files
+    replace_ip_in_template(ip_henan, template_henan, result_henan)
 
-  for genre in genres:
-    merge_by_group(genre)
-    
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
