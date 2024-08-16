ded43b1f294ede777d11f371428e015022bb1c19
welvico <veryok-abc@yahoo.com>
2024-08-17 03:02:45 +0800

    Update merge_acm.php

--- merge_acm.php
+++ merge_acm.php
@@ -1,46 +1,32 @@
 <?php
-// 读取result/ACM.txt文件内容
-$acmFile = file_get_contents('template/ACM.txt');
-$acmLines = explode("\n", $acmFile);
-
-// 读取result/all.txt文件内容
-$allFile = file_get_contents('result/all.txt');
-$allLines = explode("\n", $allFile);
+// 读取文件内容并分割成数组
+$acmFile = file('template/ACM.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$allFile = file('result/all.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 // 准备合并后的数组
 $mergedContent = [];
 
-// 处理ACM.txt的内容
-$group = '';
-foreach ($acmLines as $line) {
-    $line = trim($line); // 去除多余空格和换行符
-    if (strpos($line, '#genre#') !== false) {
-        // 新分组
-        $group = $line;
-        if (!isset($mergedContent[$group])) {
-            $mergedContent[$group] = [];
+// 函数：处理文件数组内容并存入合并数组
+function processFileContent($lines, &$mergedContent) {
+    $group = '';
+    foreach ($lines as $line) {
+        $line = trim($line); // 去除多余空格和换行符
+        if (strpos($line, '#genre#') !== false) {
+            // 新分组
+            $group = $line;
+            if (!isset($mergedContent[$group])) {
+                $mergedContent[$group] = [];
+            }
+        } elseif ($group) {
+            // 加入当前分组
+            $mergedContent[$group][] = $line;
         }
-    } elseif ($group) {
-        // 加入当前分组
-        $mergedContent[$group][] = $line;
     }
 }
 
-// 处理all.txt的内容
-$group = '';
-foreach ($allLines as $line) {
-    $line = trim($line); // 去除多余空格和换行符
-    if (strpos($line, '#genre#') !== false) {
-        // 新分组
-        $group = $line;
-        if (!isset($mergedContent[$group])) {
-            $mergedContent[$group] = [];
-        }
-    } elseif ($group) {
-        // 加入当前分组
-        $mergedContent[$group][] = $line;
-    }
-}
+// 处理ACM.txt和all.txt的内容
+processFileContent($acmFile, $mergedContent);
+processFileContent($allFile, $mergedContent);
 
 // 准备最终写入all.txt的内容
 $finalContent = '';
