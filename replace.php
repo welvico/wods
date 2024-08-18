b97b995d7be1513b3f7cf00b68caaa54cb7b4016
welvico <veryok-abc@yahoo.com>
2024-08-18 15:23:16 +0800

    Update replace.php

--- replace.php
+++ replace.php
@@ -73,7 +73,7 @@ function mergeFilesByGroup($templateDir, $resultFile) {
     // Array to store merged content by group
     $mergedContent = [];
     //Sort File
-    $sortFile = array('ACM.txt', 'Henan_327.txt', 'Henan_338.txt', 'Shanxi_CU_517.txt');
+    $sortFile = array('ACM.txt', 'Henan_327.txt', 'Henan_338.txt');
     $sortFilepath = [];
     foreach ($sortFile as $filename) {
         // Process $filename if it exists
